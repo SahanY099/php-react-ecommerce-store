7f54d8b23e322df3c164dc87f28d278679c9860b
Sahan Yashmika <88578622+SahanY099@users.noreply.github.com>
2023-11-27 16:04:54 +0530

    Fix showing signup & login errors

--- frontend/src/routes/signup.tsx
+++ frontend/src/routes/signup.tsx
@@ -5,15 +5,11 @@ import LoadingButon from "@/components/ui/loading-button";
 import { AuthContext } from "@/context/auth-provider";
 import axiosClient from "@/lib/axios-client";
 import { cn } from "@/lib/utils";
-import { CurrentUser } from "@/types/auth";
+import { AuthError, CurrentUser } from "@/types/auth";
 import { AxiosResponse } from "axios";
 import { AlertCircle } from "lucide-react";
 import { useContext, useRef, useState } from "react";
 import { Link } from "react-router-dom";
-
-interface SignupError {
-  [key: string]: Array<string>;
-}
 
 export default function Signup() {
   const [loading, setLoading] = useState(false);
@@ -21,7 +17,7 @@ export default function Signup() {
   const emailRef = useRef<HTMLInputElement>(null);
   const passwordRef = useRef<HTMLInputElement>(null);
   const passwordConfirmationRef = useRef<HTMLInputElement>(null);
-  const [errors, setErrors] = useState<SignupError>({});
+  const [errors, setErrors] = useState<AuthError | null>();
 
   const handleSubmit = (e: React.SyntheticEvent) => {
     e.preventDefault();
@@ -37,21 +33,21 @@ export default function Signup() {
       .post("/auth/signup", payload)
       .then(({ data }) => {
         const { user, token } = data;
-        setErrors({});
+        setErrors(null);
         setToken(token);
         setUser(user as CurrentUser);
       })
       .catch((err) => {
         const res = err.response as AxiosResponse;
         if (res && res.status == 422) {
-          setErrors(res.data.errors);
-          console.log(res.data.errors);
+          setErrors(res.data);
         }
       })
       .finally(() => {
         setLoading(false);
       });
   };
+
   return (
     <>
       <Link
@@ -79,19 +75,34 @@ export default function Signup() {
           onSubmit={handleSubmit}
         >
           <div className="flex flex-col gap-2">
-            {Object.keys(errors).map((key) =>
-              errors[key].map((err, i) => (
-                <Alert
-                  key={`${key} ${i}`}
-                  variant="destructive"
-                  className="flex flex-row items-start"
-                >
-                  <div className="mr-3 inline-block">
-                    <AlertCircle className="h-5 w-5" />
-                  </div>
-                  <AlertDescription>{err}</AlertDescription>
-                </Alert>
-              ))
+            {errors &&
+              errors.errors &&
+              Object.keys(errors.errors).map(
+                (key) =>
+                  errors.errors?.[key].map((err, i) => (
+                    <Alert
+                      key={`${key} ${i}`}
+                      variant="destructive"
+                      className="flex flex-row items-start"
+                    >
+                      <div className="mr-3 inline-block">
+                        <AlertCircle className="h-5 w-5" />
+                      </div>
+                      <AlertDescription>{err}</AlertDescription>
+                    </Alert>
+                  ))
+              )}
+
+            {errors && !errors.errors && errors.message && (
+              <Alert
+                variant="destructive"
+                className="flex flex-row items-start"
+              >
+                <div className="mr-3 inline-block">
+                  <AlertCircle className="h-5 w-5" />
+                </div>
+                <AlertDescription>{errors?.message}</AlertDescription>
+              </Alert>
             )}
           </div>
           <div className="flex flex-col gap-3">
